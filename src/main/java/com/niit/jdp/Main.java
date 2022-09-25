d741b57875de25524a519c457e08ad35a24d900c
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-25 23:57:30 +0530

    last commit

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,19 +1,7 @@
 package com.niit.jdp;
 
-
-import com.niit.jdp.service.DatabaseService;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int choice = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your choice: ");
-        choice = scanner.nextInt();
-
-        DatabaseService databaseService = new DatabaseService();
 
     }
 }
