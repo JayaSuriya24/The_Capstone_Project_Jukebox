9491d3c83e83f7533b9dd538b5dbb6e9f222b15c
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-26 08:51:43 +0530

    test case created

---
+++ src/test/java/com/niit/jdp/repository/PlaylistRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.niit.jdp.repository;
+
+import org.junit.jupiter.api.Test;
+
+class PlaylistRepositoryTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
---
+++ src/test/java/com/niit/jdp/repository/SongRepositoryTest.java
@@ -0,0 +1,18 @@
+package com.niit.jdp.repository;
+
+import org.junit.jupiter.api.Test;
+
+class SongRepositoryTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
