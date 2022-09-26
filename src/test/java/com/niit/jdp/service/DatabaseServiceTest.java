faaa9a85e7c6176146414ccd0494db1d4f481a00
M jaya suriya <jayasuriya2410@gmail.com>
2022-09-26 22:14:11 +0530

    DatabaseServiceTest is completed

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,11 +1,30 @@
 package com.niit.jdp.service;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 class DatabaseServiceTest {
+    DatabaseService databaseService;
+
+    @BeforeEach
+    void setUp() {
+        databaseService = new DatabaseService();
+    }
+
+    @AfterEach
+    void tearDown() {
+        databaseService = null;
+    }
 
     @Test
-    void getConnection() {
+    void getConnection() throws SQLException {
+        boolean actualOutput = databaseService.connect();
+        boolean expectedOutput = databaseService.connect();
+        Assertions.assertEquals(expectedOutput, actualOutput);
 
     }
 }
